6256fdfd0d392c3c3ec9908aef03590e7afee21b
Igor Renan <igorenansc@gmail.com>
2022-04-05 16:13:18 -0300

    Added routes/controller/views of homepage and about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root 'pages#home'
+  get 'about', to: 'pages#about'
   # Defines the root path route ("/")
   # root "articles#index"
 end
