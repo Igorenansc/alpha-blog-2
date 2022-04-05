eac4d62e9a6ea66ac2e4b61d29c90fcd201c8bb4
Igor Renan <igorenansc@gmail.com>
2022-04-05 17:45:25 -0300

    Added show and index controllers/views

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles, only: %i[show index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
